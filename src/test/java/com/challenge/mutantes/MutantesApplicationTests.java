741ac46f9a88b8627b3c3dec020505f618ddf58f
unknown <nicolasemanuel.yaminsquicciar@bbva.com>
2020-10-17 16:47:28 -0300

    Initial commit

---
+++ src/test/java/com/challenge/mutantes/MutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.mutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
