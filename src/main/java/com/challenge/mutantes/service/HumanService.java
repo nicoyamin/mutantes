509750be04614006815e5e5d5f77c064e56d6965
unknown <nicolasemanuel.yaminsquicciar@bbva.com>
2020-10-25 17:56:20 -0300

    Improving algorithm

--- src/main/java/com/challenge/mutantes/service/HumanService.java
+++ src/main/java/com/challenge/mutantes/service/HumanService.java
@@ -51,36 +51,29 @@ public class HumanService {
         }
 
         Multimap dnaMatrix = validateDna(dna);
-//        boolean isMutant= processDna(dnaMatrix);
+        boolean isMutant= processDna(dnaMatrix);
 
-        boolean isMutant = true;
+//        boolean isMutant = true;
 
         Human human = new Human();
         human.setDna(dna);
         human.setIsMutant(isMutant);
 
-        humanRepository.save(human);
+        //humanRepository.save(human);
 
         return isMutant;
     }
 
     private static Multimap<Character, Point> validateDna(String[] dna) throws ResourceFormatException {
 
-        StringBuilder stringBuilder = new StringBuilder();
-        int totalMatches = 0;
-        Set<Point> diagMatches = new HashSet<>();
-        Set<Point> horMatches = new HashSet<>();
-
-        HashMap<Character, MutableValueGraph> graphMap = new HashMap<>();
-
-
         if(dna.length < PATTERN_LENGTH) {
             throw new ResourceFormatException(DNA_SEQUENCE_TOO_SHORT_EXCEPTION);
         }
         int strandLength =  dna.length;
 
-        //Step 1 - Convert dna sequence to a matrix for traversal
+        //Step 1 - Convert dna sequence to a multiMap to isolate similar bases
         Multimap<Character,Point> dnaMatrix = ArrayListMultimap.create();
+
         for(int row = 0; row < strandLength; row++) {
             if(dna[row].length() != strandLength) {
                 throw new ResourceFormatException(INCONSISTENT_DNA_SEQUENCE_EXCEPTION);
@@ -90,90 +83,36 @@ public class HumanService {
                 if(!NITROGENOUS_BASES.contains(currentBase)) {
                     throw new ResourceFormatException(currentBase + INCORRECT_BASE_EXCEPTION);
                 }
-
-                MutableValueGraph<Point, Double> pointGraph = graphMap.get(currentBase);
                 Point newBase = new Point(row,column);
-
-                if(pointGraph == null) {
-                    MutableValueGraph<Point, Double> weightedGraph = ValueGraphBuilder.undirected().build();
-                    weightedGraph.addNode(newBase);
-                    graphMap.put(currentBase, weightedGraph);
-                } else {
-                    Set<Point> nodeSet = pointGraph.nodes();
-                    pointGraph.addNode(newBase);
-                    for(Point node : nodeSet) {
-                        if(!newBase.equals(node)) pointGraph.putEdgeValue(node, newBase, node.distance(newBase));
-                    }
-                }
                 dnaMatrix.put(currentBase, newBase);
-                }
             }
+        }
         return dnaMatrix;
     }
 
 
+    private static boolean processDna(Multimap<Character, Point> dna) {
 
-//    private static boolean processDna(Multimap dna) {
-//
-//        int detectedPattern = 0;
-//        Point previousPatternStart = null;
-//        Point previousPatternEnd = null;
-//
-//        //Step 2 - Traverse DNA matrix horizontally
-//        for(int row = 0; row < dna.length; row++) {
-//            for(int column = 0; column < dna.length; column++) {
-//                //Step 3 - Check every direction while standing in each cell
-//                for(int direction = 0; direction < 8; direction++) {
-//                    int rowDirection = row + ROWS[direction];
-//                    int columnDirection = column + COLUMNS[direction];
-//                    int matches;
-//
-//                    for(matches = 1; matches < PATTERN_LENGTH; matches++) {
-//                        //Check if out of bounds in current direction
-//                        if(rowDirection >= dna.length || rowDirection < 0
-//                                || columnDirection >= dna.length || columnDirection < 0)
-//                            break;
-//
-//                        if(dna[row][column] != dna[rowDirection][columnDirection])
-//                            break;
-//
-//                        rowDirection += ROWS[direction];
-//                        columnDirection += COLUMNS[direction];
-//                    }
-//                    //Step 4 - Check if we have a matching pattern
-//                    if (matches == PATTERN_LENGTH) {
-//
-//                        int finalRow = rowDirection-ROWS[direction];
-//                        int finalColumn = columnDirection-COLUMNS[direction];
-//
-//                        //We need to check whether this match is just a previously found one but with its direction reversed
-//                        if(detectedPattern == 0) {
-//                            previousPatternStart = new Point(row,column);
-//                            previousPatternEnd = new Point(finalRow, finalColumn);
-//                            detectedPattern++;
-//                            System.out.println("Pattern detected at (" + row +" ,"+column+")(" + finalRow+" ,"+ finalColumn +")" );
-//                            break;
-//                        }
-//
-//                        Point currentPatternStart = new Point(row, column);
-//                        Point currentPatternEnd = new Point(finalRow, finalColumn);
-//                        if(currentPatternStart.equals(previousPatternEnd) && currentPatternEnd.equals(previousPatternStart)) {
-//                            System.out.println("Duplicate pattern at (" + row +" ,"+column+")(" + finalRow+" ,"+ finalColumn +")" );
-//                            break;
-//                        }
-//
-//                        detectedPattern++;
-//                        System.out.println("Pattern detected at (" + row +" ,"+column+")(" + finalRow+" ,"+ finalColumn +")" );
-//                    }
-//                    //Step 5 - If we hit at least two matches, input is mutant DNA
-//                    if (detectedPattern == MUTANT_BASES)
-//                        return true;
-//                }
-//
-//            }
-//        }
-//
-//        //Step 5 - if we don't have at least two matches, input is human DNA
-//        return false;
-//    }
+
+
+        for(Character base : NITROGENOUS_BASES) {
+            List<Point> points = new ArrayList<>(dna.get(base));
+            for(int i=0; i < points.size(); i++) {
+                Point point1 = points.get(i);
+
+                for(int j=i+1; j < points.size()-i; j++) {
+                    Point point2 = points.get(j);
+                    if(point1.distance(point2) == 1.0 && point2.x-point1.x == 1 && point2.y-point1.y == 0) {
+
+                    }
+                }
+            }
+
+        }
+
+
+
+        //Step 5 - if we don't have at least two matches, input is human DNA
+        return false;
+    }
 }
